bcc8a7b94cac8bb6ee21cc30cf2db63d980c7348
Seb8299 <seb8299@gmail.com>
2023-05-19 22:00:30 +0200

    init minicraft

---
+++ minicraft/README.md
@@ -0,0 +1,2 @@
+# minicraft
+Minecraft in mini
---
+++ minicraft/main.js
@@ -0,0 +1,45 @@
+let map;
+
+let imgs = {};
+
+function preload() {
+
+    let tst = createImg('http://www.petwebsite.com/hamsters/hamsters_images/syrian-hamster_000008437184.jpg');
+    // imgs = {
+    //     // "water": loadImage('assets/water.png'),
+    //     // "sand": loadImage('assets/sand.png'),
+    //     // "grass": loadImage('assets/grass.png'),
+    //     // "stone": loadImage('assets/stone.png')
+    // };
+}
+
+function setup() {
+    
+    let WIDTH = displayWidth-30;
+    let HEIGHT = displayHeight-200;
+    
+    createCanvas(WIDTH, HEIGHT);
+
+    // map = new Map();
+
+    // map.draw();
+}
+
+function draw() {
+    // background(55,55,64);
+
+    // scale = 0.02;
+
+    // for (var i = 0; i < width; i++) {
+    //     for (var j = 0; j < height; j++) {
+    //         var c = noise(i*scale, j*scale)*255;
+    //         set(i, j, c);
+    //     }
+    // }
+    // updatePixels();
+}
+
+
+function mouseClicked() {
+    
+  }
